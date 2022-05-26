a084e7e6cf44d0546174cb604e8f7e8e634b05f8
07manan <07manan1850@gmail.com>
2022-05-26 11:06:45 +0530

    nav update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,21 +20,25 @@ function Navbar() {
         <a style={{ animationDelay: "400ms" }} href="#about">
           About
         </a>
-        <a style={{ animationDelay: "600ms" }} href="#skills">
-          skills
+        <a style={{ animationDelay: "600ms" }} href="#experience">
+          Experience
         </a>
-        <a style={{ animationDelay: "800ms" }} href="#contact">
-          contact
+        <a style={{ animationDelay: "800ms" }} href="#skills">
+          Skills
+        </a>
+        <a style={{ animationDelay: "1000ms" }} href="#contact">
+          Contact
         </a>
       </div>
-      <button
+      <div
+        id="ham"
         className={shownav ? "" : "nav-button"}
         onClick={() => setShownav(!shownav)}
       >
         <div className={shownav ? "bar1" : "bar1 cross1"}></div>
         <div className={shownav ? "bar2" : "bar1 cross2"}></div>
         <div className={shownav ? "bar3" : "bar1 cross3"}></div>
-      </button>
+      </div>
     </nav>
   );
 }
