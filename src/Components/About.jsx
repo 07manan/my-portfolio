d0a6c3e80e5ce152123eef6b13a02d280ccdeb1e
07manan <07manan1850@gmail.com>
2022-08-04 10:23:50 +0530

    resume update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -22,9 +22,7 @@ function About() {
       <div className="text">
         <h1 className="animateIn">Get to know Me</h1>
         <p className="animateIn-secondary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo magnam
-          delectus deleniti quae officiis quo id facere, nam eos debitis quasi
-          dicta deserunt magni maiores, at possimus. Ad, harum vitae?
+          This section is yet to be written.
         </p>
         <a
           href="/resume.pdf"
